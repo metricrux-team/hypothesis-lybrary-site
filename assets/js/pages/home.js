bce3ef04eaea7daa4bb56442da3b9650445568c5
peet1126 <pbmcdonald7@gmail.com>
2022-02-25 14:44:25 -0600

    Email popup for wait list

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-  $('.slider').slick({
+  $(".slider").slick({
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     arrows: false,
+  });
+  // triggers join wait list popup - remove when app is ready
+  $(".signup-btn").click((e) => {
+    e.preventDefault();
+    $("#popupModalCenter").modal("show");
   });
 });
